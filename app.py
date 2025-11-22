0fe76d01f839fc6f7d303f5e3bf95408ee312053
Kaushik Puttaswamy <123877618+Kaushik-Puttaswamy@users.noreply.github.com>
2025-11-22 23:24:05 +0100

    Update app.py

--- app.py
+++ app.py
@@ -11,8 +11,7 @@ def index():
             return render_template("index.html", error="No file uploaded!")
         file = request.files['resume']
         text = extract_text_from_resume(file)
-        parsed_data = parse_resume_data(text)
-    return render_template("index.html", parsed_data=parsed_data)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
